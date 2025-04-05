12cda034548c795c03ff75553806e2e0be4ebee7
PERAM DURGA SHANKAR <shankar0864213479@gmail.com>
2025-04-05 18:59:16 +0530

    Create day-1_challenge.dart

---
+++ basics_of _dart_programs/day-1_challenge.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name='shankar';
+  int age=20;
+  
+  int num1=10;
+  int num2=20;
+  int sum=num1+num2;
+  print('hello my name is $name');
+  print('my age is $age');
+  print('display the first number $num1');
+  print('display the second number $num2');
+  print('the sum of two numbers $sum');
+}
